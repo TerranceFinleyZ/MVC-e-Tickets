5b3836f7f7130556a6e878a8a3dc78e8363e6be4
zealo <zealo@LAPTOP-CLARVI89>
2023-05-23 19:15:08 -0700

    Add project files.

---
+++ MVC e-Tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_e_Tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
